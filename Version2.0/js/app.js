4180f0646e386ba8afa1968a16bea5b1617a1c3d
Dragostin <dragostij@gmail.com>
2017-12-20 12:37:44 +0200

    Front end changes

--- Version2.0/js/app.js
+++ Version2.0/js/app.js
@@ -8,38 +8,31 @@ theRef.on('value', function(snap) {
     prodData = snap.val();
     
     $.each(prodData, function(index, value) {
-        var prodPreview = '<div class="row">';
-        
-        prodPreview += '<div class="col-md-3 prodListHeader">';    
-        prodPreview += '<h2>' + value.name + '</h2>';
-        prodPreview += '</div>';
-        
-        prodPreview += '<div class="col-md-3 prodListHeader">';
-        prodPreview += '<h2>$' + value.price + '</h2>';
-        prodPreview += '</div>';
-        
-        prodPreview += '</div>';
-        
-        
-        prodPreview += '<div class="row">';
-        
-        prodPreview += '<div class="col-md-3 picFix">';
+
+        var prodPreview = '<div class="well col-sm-6 col-md-3 col-lg-4">';
+
+
+        prodPreview += '<div class="img-responsive img-thumbnail">';
         if (value.image == 'NONE') {
             prodPreview += '<img alt="No Pic">';
         } else {
             prodPreview += '<img src=' + value.image + '>';
         }
         prodPreview += '</div>';
-        
-        
-        prodPreview += '<div class="col-md-3">';
-        prodPreview += '<p>' + value.description + '</p>';
+        prodPreview += '<div class = "caption>"';
+        prodPreview += '<div class = "row>"';
+        prodPreview += '<div class="col-md-6 col-xs-6">';
+        prodPreview += '<h3>' + value.name + '</h3>';
+        prodPreview += '</div>';
+
+        prodPreview += '<div class="col-md-6 col-xs-6 price">';
+        prodPreview += '<h3><label>$' + value.price + '</label></h3>';
+        prodPreview += '<p>Status: ' + value.description + '</p>'; prodPreview += '</div>';
+
         prodPreview += '</div>';
         prodPreview += '</div>';
-        
-        
-        prodPreview += '<div class="row spacer">';
         prodPreview += '</div>';
+
         
         $(prodPreview).appendTo('#main');
 
